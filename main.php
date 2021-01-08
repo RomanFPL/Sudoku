01b70e91906b2e3c499b83cd8b1c1f7ab282d02a
Roman <roma.franko99@gmail.com>
2021-01-08 14:57:44 +0200

    Front part

---
+++ main.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sudoku</title>
+  <link rel="stylesheet" href="style-main.css">
+  <link rel="shortcut icon" href="img\sudoku.svg" type="image/x-icon">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+</head>
+
+<body>
+  <div class="full-screen">
+    <div class="user-parts">
+      <div class="game">
+      <h1>Sudoku</h1>
+      <div class="wrap-field">
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      <div class="input-item"><input type="text"></div>
+      </div>
+      <div class="gen-action btn" style="margin: 0 auto;  display: block;"><button id="check">Перевірити</button></div>
+      </div>
+    </div>
+    <div class="user-parts">
+    <div class="info">
+    <h1>Дані</h1>
+    <div class="wrap-points">
+      <div class="item-points q">
+        <h1>Час</h1>
+        <h1 data-time></h1>
+      </div>
+      <div class="item-points q">
+        <h1>Рівень</h1>
+        <h1 data-level></h1>
+      </div>
+      <div class="item-points q">
+        <h1>Ім'я</h1>
+      </div>
+    </div>
+    <div class="wrap-btn">
+    <div class="gen-action btn"><button id="new-game">Нова гра</button></div>
+    <div class="gen-action btn"><button id="pause-game">Зареєструватись</button></div>
+    <div class="gen-action btn"><button  id="exit-game">Вийти</button></div>
+    </div>
+
+    </div>
+    </div>
+  </div>
+  <?php ?>
+  <?php ?>
+  <?php require "blocks/foot.php" ?>
---
+++ script.js
@@ -0,0 +1,143 @@
+/* jshint ignore:start */
+$('#login-button').click(function () {
+  $('#login-button').fadeOut("slow", function () {
+    $("#container").fadeIn();
+    TweenMax.from("#container", .4, { scale: 0, ease: Sine.easeInOut });
+    TweenMax.to("#container", .4, { scale: 1, ease: Sine.easeInOut });
+  });
+});
+
+$(".close-btn").click(function () {
+  TweenMax.from("#container", .4, { scale: 1, ease: Sine.easeInOut });
+  TweenMax.to("#container", .4, { left: "0px", scale: 0, ease: Sine.easeInOut });
+  $("#container, #forgotten-container").fadeOut(800, function () {
+    $("#login-button").fadeIn(800);
+  });
+});
+
+/* Forgotten Password */
+$('#forgotten').click(function () {
+  $("#container").fadeOut(function () {
+    $("#forgotten-container").fadeIn();
+  });
+});
+
+/* jshint ignore:end */
+
+
+//game
+'use strict';
+const field = document.querySelectorAll('.input-item'),
+  newGame = document.getElementById('new-game'),
+  pause = document.getElementById('pause-game'),
+  check = document.getElementById('check'),
+  timer = document.querySelector('[data-time]'),
+  level = document.querySelector('[data-level]'),
+  exitGame = document.getElementById('exit-game');
+
+
+const gameTime = (elem) => {
+  const start = new Date();
+  setInterval(() => {
+    let passed = new Date() - start;
+    elem.textContent = `${(Math.floor(passed / 1000 / 60) < 10) ? "0" + Math.floor(passed / 1000 / 60) : Math.floor(passed / 1000 / 60)}:${(Math.floor(passed / 1000 % 60) < 10) ? "0" + Math.floor(passed / 1000 % 60) : Math.floor(passed / 1000 % 60)}`;
+  }, 1000);
+};
+
+const transformMatrix = (elem) => {
+  let matrix = [];
+  elem.forEach((x,y) => matrix.push(x.children[0].value));
+  let newArr = [];
+  for (let i = 0; i < matrix.length; i+=9) {
+    newArr.push(matrix.slice(i, i+9));
+  }
+  return newArr;
+};
+
+const doneOrNot = (rows) => {
+
+  let columns = [],    
+  blocks = [];
+  
+  for (let i = 0; i < 9; i++) {
+    columns[i] = [];
+    
+    for (let j = 0; j < 9; j++) {
+      let k = Math.floor(i / 3) + Math.floor(j / 3) * 3;
+      blocks[k] = blocks[k] || [];
+      
+      blocks[k].push(rows[i][j]);
+      columns[i].push(rows[j][i]);
+    }
+  }
+  
+  let isValidRow = (row) => row.slice(0).sort((a, b) => a - b).join('') == '123456789';
+  
+  let isValid = rows.every(isValidRow) && columns.every(isValidRow) && blocks.every(isValidRow);
+  
+  return isValid ? 'Finished!' : 'Try again!';
+};
+
+
+const gameBlock = {
+  easy: [
+    0, 8, 0, 3, 7, 5, 6, 0, 9,
+    3, 6, 9, 0, 4, 0, 7, 0, 0,
+    1, 0, 5, 9, 0, 2, 0, 3, 0,
+    7, 9, 8, 0, 0, 6, 0, 5, 0,
+    0, 0, 0, 4, 9, 7, 3, 8, 6,
+    6, 0, 4, 0, 5, 1, 0, 7, 2,
+    0, 4, 6, 0, 0, 2, 0, 9, 0,
+    0, 9, 0, 8, 0, 1, 7, 4, 6,
+    0, 0, 5, 6, 9, 0, 3, 8, 0]
+};
+
+newGame.addEventListener('click', () => {
+  field.forEach((x, y) => {
+    if (gameBlock.easy[y] !== 0) {
+      x.children[0].value = gameBlock.easy[y];
+    }
+  });
+  level.textContent = Object.keys(gameBlock)[0];
+  timer.textContent = "00:00";
+  gameTime(timer);
+});
+
+check.addEventListener('click', () => {
+  const row = transformMatrix(field);
+  check.textContent = doneOrNot(row);
+  setTimeout(() => {
+    check.textContent = "Перевірити";
+  }, 5000);
+});
+
+
+
+document.addEventListener('keydown', (e) => {
+  if (e.key == "ArrowUp") {
+    field.forEach((x,y) => {
+      (document.activeElement == x.children[0] && y>8) ? field[y-9].children[0].focus() : console.log('none');
+    });
+  }
+  if (e.key == "ArrowRight") {
+    console.log("work");
+  }
+  if (e.key == "ArrowLeft") {
+    console.log("work");
+  }
+  if (e.key == "ArrowDown") {
+    field.forEach((x,y) => {
+      // console.log("x is: " + typeof x, "y is: " +typeof y);
+      if (x.children[0]===document.activeElement) {
+        let d = y+9;
+        console.log(typeof d);
+        field[d].children[0].focus();
+
+        // console.log(field[y].children[0].focus());
+      }
+      // console.log(x.children[0]==document.activeElement);
+    });
+    console.log("work");
+  }
+});
+
